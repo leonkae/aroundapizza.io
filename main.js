d5d46f33c35c09071011ddde3782ec476e8e4ddb
leonkae <leon29kae@gmail.com>
2022-03-19 13:53:12 +0300

    modified main

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
-// $(".button"),
-//   click(function () {
-//     let size = document.getElementById("size").value;
-//     let toppins = document.getElementById("toppings").value;
-//     let crust = document.getElementById("crust").value;
-//   });
-
-$('.button').click(function(){
-    let size = $(".size option:selected").val();
-    let toppings = $(".toppings options:selected").val();
-    let crust = $(".crust option:selected").val();
-    let total = parseInt(size)+ parseInt(toppings)+parseInt(crust);
-    let order = 1;
-    let grandTotal = 0
-
-    $
+$(document).ready(function(){
+    $('.button-order').click(function(){
+        let size = $(".size option:selected").val();
+        let toppings = $(".toppings option:selected").val();
+        let crust = $(".crust option:selected").val();
+        let poisons = $(".poisons option:selected").val()
+        let total = parseInt(size) + parseInt(toppings) + parseInt(crust) + parseInt(poisons); 
+    })
 })
+
