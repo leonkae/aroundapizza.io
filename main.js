411a1f916b3171df63e36e58c609b2cc8b06f396
leonkae <leon29kae@gmail.com>
2022-03-19 15:36:09 +0300

    modified index,main,style

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
-$(document).ready(function(){
-    $('.button-order').click(function(){
-        let size = $(".size option:selected").val();
-        let toppings = $(".toppings option:selected").val();
-        let crust = $(".crust option:selected").val();
-        let poisons = $(".poisons option:selected").val()
-        let total = parseInt(size) + parseInt(toppings) + parseInt(crust) + parseInt(poisons); 
-    })
-})
+// $(document).ready(function () {
+//   $(".order-btn").click(function () {
+//     let size = $(".size option:selected").val();
+//     let toppings = $(".toppings option:selected").val();
+//     let crust = $(".crust option:selected").val();
+//     let poisons = $(".poisons option:selected").val();
+//     let total = 0
+//       parseInt(size) + parseInt(toppings) + parseInt(crust) + parseInt(poisons);
+//     let order = 1;
+//     let grandTotal = 0;
+
+//    $("table").show();
+   
+//   });
+
+  
+// });
+
+//  let size = $(".size option:selected").val();
+//  let toppings = $(".toppings option:selected").val();
+//  let crust = $(".crust option:selected").val();
+//  let poisons = $(".poisons option:selected").val();
+//  let total =
+//    parseInt(size) + parseInt(toppings) + parseInt(crust) + parseInt(poisons);
+//  let order = 1;
+//  let grandTotal = 0;
+
+//  console.log (grandTotal)
+
+//  document.addEventListener("click", function(){
+//     let size = $(".size option:selected").val();
+//     let toppings = $(".toppings option:selected").val();
+//     let crust = $(".crust option:selected").val();
+//     let poisons = $(".poisons option:selected").val();
+//     let total =
+//       parseInt(size) + parseInt(toppings) + parseInt(crust) + parseInt(poisons);
+//     let order = 1;
+//     let grandTotal = 0;
+//  })
+
+
 
