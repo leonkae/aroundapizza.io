fe8258045b03e83a66ecdf086f8deca6107e7fa1
leonkae <leon29kae@gmail.com>
2022-03-19 10:10:30 +0300

    added images

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-let size = document.getElementById("size");
-let toppins = document.getElementById("toppings");
-let crust =document.getElementById("crust");
+// $(".button"),
+//   click(function () {
+//     let size = document.getElementById("size").value;
+//     let toppins = document.getElementById("toppings").value;
+//     let crust = document.getElementById("crust").value;
+//   });
 
+$('.button').click(function(){
+    let size = $(".size option:selected").val();
+    let toppings = $(".toppings options:selected").val();
+    let crust = $(".crust option:selected").val();
+    let total = parseInt(size)+ parseInt(toppings)+parseInt(crust);
+    let order = 1;
+    let grandTotal = 0
 
+    $
+})
