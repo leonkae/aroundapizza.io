5b98365727b4f2e5577a8d843daeb483f0b9d67b
leonkae <leon29kae@gmail.com>
2022-03-19 23:44:24 +0300

    modified main.js

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+let size = document.getElementById("size");
+let toppings = document.getElementById("toppings");
+let crust = document.getElementById("crust");
+let poisons = document.getElementById("poisons");
+let total = document.getElementById("totalone");
+let totalArray = [];
+
+let sizeone = document.getElementById("sizeone");
+let toppingsone = document.getElementById("toppingsone");
+let crustone = document.getElementById("crustone");
+let poisonsone = document.getElementById("poisonsone");
+
+let form = document.getElementById("form-wrapper");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let sizeValue = size.value;
+  let toppingsValue = toppings.value;
+  let crustValue = crust.value;
+  let poisonsValue = poisons.value;
+  let totaltwo =
+    parseInt(sizeValue) +
+    parseInt(toppingsValue) +
+    parseInt(crustValue) +
+    parseInt(poisonsValue);
+  totalArray.push(totaltwo);
+  totalArray.reduce(sum,num => sum + num);
+
+  sizeone.textContent = sizeValue;
+  toppingsone.textContent = toppingsValue;
+  crustone.textContent = crustValue;
+  poisonsone.textContent = poisonsValue;
+  total.textContent = totaltwo;
+
+  let table = document.getElementById("table-content");
+  table.classList.remove("hide");
+});
+
 // $(document).ready(function () {
 //   $(".order-btn").click(function () {
 //     let size = $(".size option:selected").val();
@@ -10,10 +48,9 @@
 //     let grandTotal = 0;
 
 //    $("table").show();
-   
+
 //   });
 
-  
 // });
 
 //  let size = $(".size option:selected").val();
@@ -37,6 +74,3 @@
 //     let order = 1;
 //     let grandTotal = 0;
 //  })
-
-
-
