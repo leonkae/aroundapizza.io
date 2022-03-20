8cfb00f93e9c6a90a91d698d79397aad555eb2aa
leonkae <leon29kae@gmail.com>
2022-03-20 21:59:22 +0300

    updates& upgrades

--- main.js
+++ main.js
@@ -6,11 +6,14 @@ let total = document.getElementById("totalone");
 let totalArray = [];
 let table = document.getElementById("table-content");
 let ordernum = document.getElementById("order-num");
+let delivery = document.getElementById("delivery");
+// console.log(delivery);
 
 let sizeone = document.getElementById("sizeone");
 let toppingsone = document.getElementById("toppingsone");
 let crustone = document.getElementById("crustone");
 let poisonsone = document.getElementById("poisonsone");
+let deliveryone = document.getElementById("deliveryone");
 
 let form = document.getElementById("form-wrapper");
 form.addEventListener("submit", function (event) {
@@ -20,11 +23,13 @@ form.addEventListener("submit", function (event) {
   let toppingsValue = toppings.value;
   let crustValue = crust.value;
   let poisonsValue = poisons.value;
+  let deliveryValue = delivery.value;
   let totaltwo =
     parseInt(sizeValue) +
     parseInt(toppingsValue) +
     parseInt(crustValue) +
-    parseInt(poisonsValue);
+    parseInt(poisonsValue) +
+    parseInt(deliveryValue);
   totalArray.push(totaltwo);
   let tallarr = totalArray.reduce((sum, num) => sum + num);
 
@@ -33,11 +38,12 @@ form.addEventListener("submit", function (event) {
   toppingsone.textContent = toppingsValue;
   crustone.textContent = crustValue;
   poisonsone.textContent = poisonsValue;
+  deliveryone.textContent = deliveryValue;
   total.textContent = tallarr;
-//   let newRow = document.createElement("tr"); 
-//   let newtd = newRow.appendChild("td")
-//   newtd.textContent = sizeValue
-
+  
+  //   let newRow = document.createElement("tr");
+  //   let newtd = newRow.appendChild("td")
+  //   newtd.textContent = sizeValue
 
   table.classList.remove("hide");
 });
@@ -46,41 +52,3 @@ let onemore = document.getElementById("one-more");
 onemore.addEventListener("click", function () {
   table.classList.add("hide");
 });
-// $(document).ready(function () {
-//   $(".order-btn").click(function () {
-//     let size = $(".size option:selected").val();
-//     let toppings = $(".toppings option:selected").val();
-//     let crust = $(".crust option:selected").val();
-//     let poisons = $(".poisons option:selected").val();
-//     let total = 0
-//       parseInt(size) + parseInt(toppings) + parseInt(crust) + parseInt(poisons);
-//     let order = 1;
-//     let grandTotal = 0;
-
-//    $("table").show();
-
-//   });
-
-// });
-
-//  let size = $(".size option:selected").val();
-//  let toppings = $(".toppings option:selected").val();
-//  let crust = $(".crust option:selected").val();
-//  let poisons = $(".poisons option:selected").val();
-//  let total =
-//    parseInt(size) + parseInt(toppings) + parseInt(crust) + parseInt(poisons);
-//  let order = 1;
-//  let grandTotal = 0;
-
-//  console.log (grandTotal)
-
-//  document.addEventListener("click", function(){
-//     let size = $(".size option:selected").val();
-//     let toppings = $(".toppings option:selected").val();
-//     let crust = $(".crust option:selected").val();
-//     let poisons = $(".poisons option:selected").val();
-//     let total =
-//       parseInt(size) + parseInt(toppings) + parseInt(crust) + parseInt(poisons);
-//     let order = 1;
-//     let grandTotal = 0;
-//  })
